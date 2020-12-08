1ea3f34704a9cc9499c903d43b2be974d238e680
Kellie Mullan <73853557+mullanke@users.noreply.github.com>
2020-12-07 21:36:00 -0500

    Add files via upload

---
+++ Python-Challenge/PyBank/main.py
@@ -0,0 +1,105 @@
+# Dependencies
+import pandas as pd
+
+# Create references
+budget_csv = "Resources/budget_data.csv"
+
+# Read the csv and print
+budget_df = pd.read_csv(budget_csv)
+print(budget_df)
+
+# Calculate the total number of months
+num_months = budget_df["Date"].count()
+print(f"Total number of months: {num_months}")
+    #Total number of months: 86
+
+# Calculate the net total amount of "Profit/Losses" over the entire period
+net_total = budget_df["Profit/Losses"].sum()
+print(f"Net total amount of profit/losses: ${net_total}")
+    # Net total amount of profit/losses: $38382578
+
+# Calculate the average of the changes in "Profit/Losses" over the entire period
+
+# Subtract the starting value from the ending value for each value
+diff = budget_df['Profit/Losses'].diff()
+
+# Add new column for difference
+budget_df["Difference"] = diff
+
+# Sum up the difference values in the Difference column
+sum_of_diff = budget_df["Difference"].sum()
+
+# Find number of diff values in the Difference column and calculate average
+num_of_diff = budget_df["Difference"].count()
+avg_of_change = sum_of_diff / num_of_diff
+avg_of_change = round(avg_of_change, 2)
+
+print(f"Average Change: ${avg_of_change}")
+    #Average Change: $-2315.12
+
+# Calculate the greatest increase in profits (date and amount) over the entire period
+
+# Put in descending order
+budget_df_descending = budget_df.sort_values("Difference", ascending=False)
+
+# Reset index
+budget_df_descending = budget_df_descending.reset_index(drop=True)
+
+# Greatest increase amount
+greatest_increase_amount = budget_df_descending.iloc[0]["Difference"]
+
+# Greatest increase date
+greatest_increase_date = budget_df_descending.iloc[0]["Date"]
+
+print(f"Greatest increase amount: ${greatest_increase_amount}")
+print(f"Greatest increase date: {greatest_increase_date}")
+
+budget_df_descending.head()
+    # Greatest increase amount: $1926159.0
+    # Greatest increase date: Feb-2012
+
+# Calculate the greatest decrease in losses (date and amount) over the entire period
+
+# Put in ascending order
+budget_df_ascending = budget_df.sort_values("Difference")
+
+# Reset index
+budget_df_ascending = budget_df_ascending.reset_index(drop=True)
+
+# Greatest decrease amount
+greatest_decrease_amount = budget_df_ascending.iloc[0]["Difference"]
+
+# Greatest decrease date
+greatest_decrease_date = budget_df_ascending.iloc[0]["Date"]
+
+print(f"Greatest decrease amount: ${greatest_decrease_amount}")
+print(f"Greatest decrease date: {greatest_decrease_date}")
+
+budget_df_ascending.head()
+    # Greatest decrease amount: $-2196167.0
+    # Greatest decrease date: Sep-2013
+    
+# Print the analysis to the terminal
+print("-------------------------------")
+print("Financial Analysis")
+print("-------------------------------")
+print(f"Total Months: {num_months}")
+print("Total: ${:,.2f}".format(net_total))
+print("Average Change: ${:,.2f}".format(avg_of_change))
+print("Greatest Increase in Profits: " + greatest_increase_date + ", ${:,.2f}".format(greatest_increase_amount))
+print("Greatest Decrease in Profits: " + greatest_decrease_date + ", ${:,.2f}".format(greatest_decrease_amount))
+
+
+with open("PyBank_Results.txt", 'w') as file:
+
+    file.write("---------------------------------------------------\r\n")
+    file.write("Financial Analysis\r\n")
+    file.write(
+        "----------------------------------------------------------\r\n")
+    file.write(f"Total Months: {num_months}\r\n")
+    file.write("Total: ${:,.2f}\r\n".format(net_total))
+    file.write("Average Change: ${:,.2f}\r\n".format(avg_of_change))
+    file.write("Greatest Increase in Profits: " + greatest_increase_date +
+               ", ${:,.2f}\r\n".format(greatest_increase_amount))
+    file.write("Greatest Decrease in Profits: " + greatest_decrease_date +
+               ", ${:,.2f}\r\n".format(greatest_decrease_amount))
---
+++ Python-Challenge/PyPoll/main2.py
@@ -0,0 +1,108 @@
+# Dependencies
+import pandas as pd
+import os
+
+# Create reference to the csv file and read
+csv_file = "Resources/election_data.csv"
+election_data_df = pd.read_csv(csv_file)
+
+# Print
+print(election_data_df)
+
+# The total number of votes cast
+num_votes = election_data_df["Voter ID"].count()
+
+print(f"Total number of votes cast: {num_votes}")
+    # Total number of votes cast: 3521001
+
+# A complete list of candidates who received votes
+list_candidates = election_data_df["Candidate"].unique()
+print("List of candidates who have received votes:")
+print(list_candidates)
+    # ['Khan' 'Correy' 'Li' "O'Tooley"]
+    
+# Calculate the total number of votes each candidate won
+num_votes_list = []
+for candidate in list_candidates:
+    votes_for_candidate_df = election_data_df.loc[election_data_df["Candidate"] == candidate]
+    num_votes_candidate = votes_for_candidate_df["Voter ID"].count()
+    num_votes_list.append(num_votes_candidate)
+
+print("Number of votes each candidate won:")
+print(num_votes_list)
+    # [2218231, 704200, 492940, 105630]
+    
+# Calculate the percentage of votes each candidate won
+percent_votes_list = []
+for vote_count in num_votes_list:
+    percent_votes_candidate = (vote_count / num_votes) * 100
+    percent_votes_candidate = round(percent_votes_candidate, 2)
+    percent_votes_list.append(percent_votes_candidate)
+
+print("Percentage of votes each candidate won:")
+print(percent_votes_list)
+    # [63.0, 20.0, 14.0, 3.0]
+    
+# Calculate the winner of the election based on popular vote.
+
+# Make dictionary of lists
+election_results_dict = {
+    "Candidates": list_candidates,
+    "Number of Votes": num_votes_list,
+    "Percentage of Votes (%)": percent_votes_list
+    
+}
+
+# Create dataframe from dictionary of lists
+election_results_df = pd.DataFrame(election_results_dict)
+
+# Sort results in descending order to determine winner
+election_results_descending_df = election_results_df.sort_values(
+    "Number of Votes", ascending=False)
+
+# Reset index
+election_results_descending_df = election_results_descending_df.reset_index(
+    drop=True)
+
+# Set a variable for winner
+winner = election_results_descending_df.iloc[0]['Candidates']
+print(f"Winner: {winner}")
+
+# Format values
+election_results_descending_df["Percentage of Votes (%)"] = election_results_descending_df["Percentage of Votes (%)"].astype(
+    str) + '%'
+election_results_descending_df["Number of Votes"] = election_results_descending_df["Number of Votes"].astype(
+    float)
+
+# Print in descending order
+election_results_descending_df
+
+# Print analysis
+print("---------------------------------------------------------------")
+print("Election Results")
+print("---------------------------------------------------------------")
+print("Total votes: {:,.0f}".format(num_votes))
+print("---------------------------------------------------------------")
+print(election_results_descending_df.to_string(index=False))
+print("---------------------------------------------------------------")
+print(f"Winner: {winner}")
+print("---------------------------------------------------------------")
+
+# Export text file
+with open("election_results.txt", 'w') as file:
+
+    file.write(
+        "---------------------------------------------------------------\r\n")
+    file.write("Election Results\r\n")
+    file.write(
+        "---------------------------------------------------------------\r\n")
+    file.write("Total votes: {:,.0f}".format(num_votes) + "\r\n")
+    file.write(
+        "---------------------------------------------------------------\r\n")
+    file.write(election_results_descending_df.to_string(
+        index=False) + "\r\n")
+    file.write(
+        "---------------------------------------------------------------\r\n")
+    file.write(f"Winner: {winner}\r\n")
+    file.write(
+        "---------------------------------------------------------------\r\n")
